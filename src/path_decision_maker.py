97db2fea0ba448edd994cd594fa6057a1a698fc0
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2025-12-22 03:46:32 -0200

    Modified the file to have all the necesary data to return to have more precission

--- src/path_decision_maker.py
+++ src/path_decision_maker.py
@@ -22,26 +22,25 @@ class FileSizeEstimator:
         total_filas= class_overhead_parquet.metadata.num_rows
         
         #resources available and estimated
-        estimated_memory= (overhead_estimated*uncompressed_data_size) / (1024**3)
-        memoria_disponible=psutil.virtual_memory().available/(1024**3)
-        total_memory=psutil.virtual_memory().total/(1024**3)
+        estimated_memory= (overhead_estimated*uncompressed_data_size) 
+        memoria_disponible=psutil.virtual_memory().available
+        total_memory=psutil.virtual_memory().total
         
         #margin of safety
         safety_memory= total_memory*self.os_margin
-        usable_ram= memoria_disponible-safety_memory
         
-        ratio= estimated_memory/usable_ram
+        ratio= estimated_memory/memoria_disponible
         
         return {
             'os_margin': self.os_margin,
-            'ratio': round(ratio, 3), 
-            'overhead_estimado':round(overhead_estimated, 3), 
-            'safety_memory':round(safety_memory, 3),
-            'archivo_descomprimido_gb': round(uncompressed_data_size/(1024**3), 3),
+            'ratio': ratio,  
+            'overhead_estimado':overhead_estimated, 
+            'safety_memory':safety_memory, 
+            'archivo_descomprimido': uncompressed_data_size,
             'total_de_filas_gb': total_filas, 
-            'memoria_total_estimada_gb':round(estimated_memory, 3), 
-            'memoria_disponible':round(memoria_disponible, 3), 
-            'total_memory':round(total_memory, 3)
+            'memoria_total_estimada':estimated_memory,
+            'memoria_disponible':memoria_disponible, 
+            'total_memory':total_memory
         }
     
     def estimate_csv_size(self, 
@@ -53,25 +52,24 @@ class FileSizeEstimator:
         csv_overhead= csv_overhead_class.overhead_csv()
         
         #resources and estimated resources
-        estimated_memory= (num_rows*csv_overhead*bytes_per_column) / (1024**3)
-        memoria_disponible= psutil.virtual_memory().available/(1024**3)
-        total_memory=psutil.virtual_memory().total/(1024**3)
+        estimated_memory= (num_rows*csv_overhead*bytes_per_column) 
+        memoria_disponible= psutil.virtual_memory().available
+        total_memory=psutil.virtual_memory().total
         
         #margin of safety
         safety_memory= total_memory*self.os_margin
-        usable_ram= memoria_disponible-safety_memory
         
-        ratio= estimated_memory/usable_ram
+        ratio= estimated_memory/memoria_disponible
         
         return {
             'os_margin': self.os_margin,
-            'ratio': round(ratio, 3),
+            'csv_overhead': csv_overhead,  
+            'ratio': ratio, 
             'total_rows':num_rows, 
-            'bytes_por_columna':bytes_per_column,
-            'safety_memory':round(safety_memory, 3),
-            'memoria_total_estimada_gb':round(estimated_memory, 3), 
-            'memoria_disponible':round(memoria_disponible, 3), 
-            'total_memory':round(total_memory, 3)
+            'safety_memory':safety_memory, 
+            'memoria_total_estimada_gb':estimated_memory, 
+            'memoria_disponible':memoria_disponible, 
+            'total_memory':total_memory
         }
 
 class PipelineEstimatedSizeFiles: 
@@ -86,7 +84,7 @@ class PipelineEstimatedSizeFiles:
             overhead_csv= CsvOverhead(path=self.archivo, n_rows_sample=self.n_rows_sample)
             resources_csv= self.estimator.estimate_csv_size(csv_overhead_class=overhead_csv, csv_overhead_estimator_class=overhead_csv_class)
             
-            resources_csv['tamaño_archivo_gb']=round(self.archivo.stat().st_size/(1024**4), 3)
+            resources_csv['tamaño_archivo_gb']=self.archivo.stat().st_size
             if resources_csv['ratio'] <= 0.65: 
                 resources_csv['decision']= 'eager'
             elif resources_csv['ratio'] <= 2.0:
@@ -98,7 +96,8 @@ class PipelineEstimatedSizeFiles:
             overhead_parquet= ParquetOverheadEstimator(archivo=self.archivo, n_rows_sample=self.n_rows_sample)
             resources_parquet=self.estimator.estimate_parquet_size(class_overhead_parquet=overhead_parquet)
             
-            resources_parquet['tamaño_archivo']=round(self.archivo.stat().st_size/(1024**3), 3)
+            resources_parquet['parquet_metadata']= overhead_parquet.metadata
+            resources_parquet['tamaño_archivo']=self.archivo.stat().st_size
             if resources_parquet['ratio'] <= 0.65: 
                 resources_parquet['decision']= 'eager'
             elif resources_parquet['ratio'] <= 2.0:
